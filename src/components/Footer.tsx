fcc11dd12a0e9ba8c4acd94835efed39231c0068
Ken <yongkang.cheng@mail.utoronto.ca>
2025-02-03 19:11:42 -0500

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,35 +5,41 @@ import './Footer.css';
 function Footer() {
     return (
         <div className='footer'>
-            <div className='footer-logo'>
-                <img src="/assets/voluntrack_bg.png" alt="voluntrack logo" />
+            <div className='footer-container'>
+                <div className='footer-logo'>
+                    <img src="/assets/logo.png" alt="voluntrack logo" />
+                </div>
+                <div className='footer-left'>
+                    <h2>Voluntrack</h2>
+                    <p>Connecting Futures: Empowering Youth and Communities through Innovative Volunteering.</p>
+                    <b>Get in touch</b>
+                    <p>info@Voluntracks.com</p>
+                    <p>CC: 1550385-0</p>
+                    <p>(674)-674-6127</p>
+                </div>
+                <div className='footer-right'>
+                    <div className='footer-about'>
+                        <b>About</b>
+                        <p>Team</p>
+                        <p>History</p>
+                        <p>Careers</p>
+                    </div>
+                    <div className='footer-privacy'>
+                        <b>Privacy</b>
+                        <p>Privacy Policy</p>
+                        <p>Terms and Conditions</p>
+                    </div>
+                    <div className='footer-social'>
+                        <b>Social</b>
+                        <p>Facebook</p>
+                        <p>Instagram</p>
+                        <p>Twitter/X</p>
+                    </div>
+                </div>
             </div>
-            <div className='footer-left'>
-                <h2>Voluntrack</h2>
-                <p>Connecting Futures: Empowering Youth and Communities through Innovative Volunteering.</p>
-                <b>Get in touch</b>
-                <p>info@Voluntracks.com</p>
-                <p>CC: 1550385-0</p>
-                <p>(674)-674-6127</p>
-            </div>
-            <div className='footer-right'>
-                <div className='footer-about'>
-                    <b>About</b>
-                    <p>Team</p>
-                    <p>History</p>
-                    <p>Careers</p>
-                </div>
-                <div className='footer-privacy'>
-                    <b>Privacy</b>
-                    <p>Privacy Policy</p>
-                    <p>Terms and Conditions</p>
-                </div>
-                <div className='footer-social'>
-                    <b>Social</b>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
-                    <p>Twitter/X</p>
-                </div>
+            <div className='footer-bottom'>
+                <p>©VolunTrack Org. is a registered Canadian Not-for-profit #716778212</p>
+                <p>Copyright © 2024 VolunTrack Org.</p>
             </div>
         </div>
     )
